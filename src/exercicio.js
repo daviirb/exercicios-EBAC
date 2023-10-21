05bc2dc296dcf7e36381e8f12fef666f457ed212
Davi Ribeiro <davi.rb@hotmail.com>
2023-10-21 13:12:52 -0300

    first commit

---
+++ src/exercicio.js
@@ -0,0 +1,39 @@
+const listaAlunos = [
+  {
+    nome: "Ana",
+    nota: 9.0
+  },
+  {
+    nome: "Jose",
+    nota: 6.5
+  },
+  {
+    nome: "Marcos",
+    nota: 7.0
+  },
+  {
+    nome: "Vitor",
+    nota: 4.0
+  },
+  {
+    nome: "Karla",
+    nota: 10
+  },
+  {
+    nome: "Melissa",
+    nota: 4.5
+  },
+  {
+    nome: "Carlos",
+    nota: 6.0
+  }
+]
+
+function filtraAlunos(listaAlunos) {
+  return listaAlunos.nota >= 6;
+}
+
+const novaLista = listaAlunos.filter(filtraAlunos)
+
+console.log(novaLista)
+
