1a55f507ace5687aa10697201fd4080bcb4410da
Davi Ribeiro <davi.rb@hotmail.com>
2023-10-22 13:07:27 -0300

    exercicio pronto

---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+const multiplicador = (a, b) => a * b;
+console.log(multiplicador(220, 10));
+const dizOla = (nome) => `Olá ${nome}`;
+console.log(dizOla("Davi"));
---
+++ main.ts
@@ -0,0 +1,5 @@
+const multiplicador = (a: number, b: number): number => a * b
+console.log(multiplicador(220, 10))
+
+const dizOla = (nome: string): string => `Olá ${nome}`
+console.log(dizOla("Davi"))
